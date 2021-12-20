9775681c122ffaba06157d5fc9dd77fe1f69a576
Tommaso <tommaso.macco@gmail.com>
2021-12-20 21:29:38 +0100

    Snack4 done

---
+++ Snack4/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack4</title>
+    <?php
+        $min = 10;
+        $max= 1000;
+        $nItems = 15;
+    ?>
+
+
+</head>
+<body>
+    <!-- Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta -->
+
+    <?php
+        getArrayOfIntegers($min, $max, $nItems);
+        {
+            $arrayNumber = [];
+
+            while (count($arrayNumber) < $nItems) {
+                $rnd = rand($min, $max);
+                if (in_array($rnd, $arrayNumber)) {
+                    $arrayNumber[] = $rnd;
+                }
+            }
+            return $arrayNumber;
+        }
+    ?>
+    <div>
+        <?php
+            for ($i=0; $i < count($arrayNumber) ; $i++) { 
+                echo '<h2>' . $arrayNumber[$i] . '<h2>';
+            }
+        ?>
+    </div>
+</body>
+</html>
